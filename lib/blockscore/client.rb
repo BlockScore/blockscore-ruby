abf85f2a051adbb4dd87373ce87d1bc947c04287
Alain Meier <me@alainmeier.com>
2014-02-22 20:45:20 -0800

    Gemfile lock, client.rb

---
+++ lib/blockscore/client.rb
@@ -0,0 +1,40 @@
+module Blockscore
+  class Client
+    include HTTParty
+
+    def initialize(api_key, options = {})
+      @api_key = api_key
+      @auth = { username: @api_key, password: "" }
+
+      options[:base_uri] ||= "https://api.blockscore.com"
+      options[:headers] = { 'Accept' => 'application/vnd.blockscore+json;version=2' }
+      
+      options.each do |k,v|
+        self.class.send k, v
+      end
+    end
+
+    def get_verifications
+      self.get '/verifications'
+    end
+
+    def get_verification(id)
+      self.get '/verifications/' + id.to_s
+    end
+
+    def create_verification(options = {})
+      self.post '/verifications/', options
+    end
+
+    def get(path, options = {})
+      options = { body: options, basic_auth: @auth }
+      puts options
+      self.class.get(path, options)
+    end
+
+    def post(path, options = {})
+      options = { body: options, basic_auth: @auth }
+      self.class.post(path, options)
+    end
+  end
+end
