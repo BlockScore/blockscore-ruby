0792d6c8bfcaec266365f8e32815959528bd603c
Alain Meier <me@alainmeier.com>
2014-06-14 22:59:29 -0700

    Clean up tests

--- test/test_blockscore.rb
+++ test/test_blockscore.rb
@@ -3,67 +3,72 @@ require 'helper'
 class TestBlockScore < Test::Unit::TestCase
   context "a verification" do
     setup do
-      @blockscore = BlockScore::Client.new("YOUR_API_KEY", version = 2)
+      # If you'd like to run the test suite, fill in your API key,
+      # a verification ID and a question set ID below.
+      @client = BlockScore::Client.new("Your API key", version = 2)
+
+      @verification_id = "Your verification ID"
+      @question_set_id = "Your question set ID"
     end
 
     should "return a list of verifications" do
-      assert_equal 200, @blockscore.verification.all.code
+      response = @client.verification.all
+      assert_equal 200, response.code
     end
 
     should "return count = 2 verifications" do
-      assert_equal 200, @blockscore.verification.all(count=2).code
+      response = @client.verification.all(count = 2)
+      assert_equal 200, response.code
     end
 
     should "return count=2 offset=2 verifications" do
-      assert_equal 200, @blockscore.verification.all(count=2, offset=2).code
+      response = @client.verification.all(count = 2, offset = 2)
+      assert_equal 200, response.code
     end
 
     should "return a single verification" do
-      assert_equal 200, @blockscore.verification.retrieve("YOUR_VERIFICATION_ID").code
+      response = @client.verification.retrieve(@verification_id)
+      assert_equal 200, response.code
     end
 
     should "return create a verification" do
-      @type = "us_citizen"
-      @date_of_birth = "1975-01-01"
-      
-      @identification = {
-        ssn: "0000"
-      }
-      
-      @name = {
-        first: "John",
-        middle: nil,
-        last: "Doe"
+      verification_params = {
+        type: "us_citizen",
+        date_of_birth: "1975-01-01",
+        identification: {
+          ssn: "0000"
+        },
+        name: {
+          first: "John",
+          middle: "P",
+          last: "Doe"
+        },
+        address: {
+          street1: "1 Infinite Loop",
+          street2: nil,
+          city: "Cupertino",
+          state: "CA",
+          postal_code: "95014",
+          country: "US"
+        }
       }
 
-      @address = {
-        street1: "1 Infinite Loop",
-        street2: nil,
-        city: "Cupertino",
-        state: "CA",
-        postal_code: "95014",
-        country: "US"
-      }
+      response = @client.verification.create(verification_params)
 
-      assert_equal 201, @blockscore.verification
-                              .create(
-                                @type,
-                                @date_of_birth,
-                                @identification, 
-                                @name, 
-                                @address
-                              ).code
+      assert_equal 201, response.code
     end
 
     should "return create a question set" do
-      @verification_id = "YOUR_VERIFICATION_ID"
+      response = @client.question_set.create(@verification_id)
+      assert_equal 201, response.code
+    end
 
-      assert_equal 201, @blockscore.question_set.create(@verification_id).code
+    should "return a single question set" do
+      response = @client.question_set.retrieve(@question_set_id, @verification_id)
+      assert_equal 201, response.code
     end
 
     should "return a score for the question set" do
-      @verification_id = "YOUR_VERIFICATION_ID"
-      @question_set_id = "YOUR_QUESTION_SET_ID"
       @answers = [
           {
             question_id: 1,
@@ -87,12 +92,13 @@ class TestBlockScore < Test::Unit::TestCase
           }
         ]
 
-      assert_equal 201, @blockscore.question_set
-                              .score(
+      response = @client.question_set.score(
                                 @verification_id,
                                 @question_set_id,
                                 @answers
-                              ).code
+                              )
+
+      assert_equal 201, response.code
     end
   end
 end
