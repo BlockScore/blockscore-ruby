dc6450ffadfdc8fe6faaa5da21d45af9c2111023
Alain Meier <me@alainmeier.com>
2014-06-29 22:28:17 -0700

    Company endpoint

---
+++ lib/blockscore/company.rb
@@ -0,0 +1,35 @@
+module BlockScore
+  class Company
+
+    def initialize(client)
+      @client = client
+    end
+
+    # 
+    # /companies POST
+    #
+    def create(options = {})
+      response = @client.post '/companies', options
+    end
+
+    # 
+    # /companies/:id GET
+    #
+    def retrieve(id, options = {})
+      body = (options.include? :query) ? options[:body] : {}
+      response = @client.get "/companies/#{id.to_s}", body
+    end
+
+    # 
+    # '/companies' GET
+    #
+    def all(count = nil, offset = nil, options = {})
+      body = (options.include? :body) ? options[:body] : {}
+
+      body[:count] = count
+      body[:offset] = offset
+
+      @client.get '/companies', body
+    end
+  end
+end
