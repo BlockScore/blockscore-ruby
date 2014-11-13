408d86468803f0635fe1ea50f5e5533f40050300
Connor Jacobsen <jacobsen.connor@gmail.com>
2014-11-13 15:22:14 -0500

    change indentation to use spaces

--- lib/blockscore/verification.rb
+++ lib/blockscore/verification.rb
@@ -1,36 +1,36 @@
 module BlockScore
-	class Verification
+  class Verification
 
-		def initialize(client)
-			@client = client
-		end
+    def initialize(client)
+      @client = client
+    end
 
-		# 
-		# /verifications POST
-		#
-		def create(options = {})
-			response = @client.post '/verifications', options
-		end
+    # 
+    # /verifications POST
+    #
+    def create(options = {})
+      response = @client.post '/verifications', options
+    end
 
-		# 
-		# /verifications/:id GET
-		#
-		# id -
-		def retrieve(id, options = {})
-			body = (options.include? :query) ? options[:body] : {}
-			response = @client.get "/verifications/#{id.to_s}", body
-		end
+    # 
+    # /verifications/:id GET
+    #
+    # id -
+    def retrieve(id, options = {})
+      body = (options.include? :query) ? options[:body] : {}
+      response = @client.get "/verifications/#{id.to_s}", body
+    end
 
-		# 
-		# '/verifications' GET
-		#
-		def all(count = nil, offset = nil, options = {})
-			body = (options.include? :body) ? options[:body] : {}
+    # 
+    # '/verifications' GET
+    #
+    def all(count = nil, offset = nil, options = {})
+      body = (options.include? :body) ? options[:body] : {}
 
-			body[:count] = count
-			body[:offset] = offset
+      body[:count] = count
+      body[:offset] = offset
 
-			@client.get '/verifications', body
-		end
-	end
+      @client.get '/verifications', body
+    end
+  end
 end
