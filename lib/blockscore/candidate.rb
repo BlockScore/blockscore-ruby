7ace30c341cb449f9c28f7efc64ea0fdf458aaf2
Connor Jacobsen <jacobsen.connor@gmail.com>
2014-11-14 19:27:14 -0500

    rename watchlist_candidate to candidate

---
+++ lib/blockscore/candidate.rb
@@ -0,0 +1,47 @@
+module BlockScore
+  class Candidate
+    def initialize(client)
+      @client = client
+    end
+
+    # POST https://api.blockscore.com/candidates
+    def create(options = {})
+      response = @client.post '/candidates', options
+    end
+
+    # PATCH https://api.blockscore.com/candidates/{CANDIDATE_ID}
+    def edit(candidate_id, options = {})
+      response = @client.put "/candidates/#{candidate_id}", options 
+    end
+
+    # DELETE https://api.blockscore.com/candidates/{CANDIDATE_ID}
+    def delete(candidate_id)
+      response = @client.delete "/candidates/#{candidate_id}"
+    end
+
+    # GET https://api.blockscore.com/candidates/{CANDIDATE_ID}
+    def retrieve(candidate_id)
+      response = @client.get "/candidates/#{candidate_id}"
+    end
+
+    # GET https://api.blockscore.com/candidates
+    def all(count = nil, offset = nil, options = {})
+      body = (options.include? :body) ? options[:body] : {}
+
+      body[:count] = count
+      body[:offset] = offset
+
+      @client.get '/candidates', body
+    end
+
+    # GET https://api.blockscore.com/candidates/:id/history
+    def history(candidate_id)
+      response = @client.get "/candidates/#{candidate_id}/history"
+    end
+
+    # GET https://api.blockscore.com/watchlist_candidates/:id/hits
+    def hits(candidate_id)
+      response = @client.get "/candidates/#{candidate_id}/hits"
+    end
+  end
+end
