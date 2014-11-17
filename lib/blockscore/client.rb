65e48c9df816cc195c96f60928968c6253ab7736
Alain Meier <me@alainmeier.com>
2014-11-17 02:34:27 -0800

    Add user agent

--- lib/blockscore/client.rb
+++ lib/blockscore/client.rb
@@ -8,14 +8,17 @@ module BlockScore
       @api_key = api_key
       @auth = { :username => @api_key, :password => "" }
       @people = BlockScore::People.new(self)
-      @question_set = BlockScore::QuestionSets.new(self)
+      @question_sets = BlockScore::QuestionSets.new(self)
       @companies = BlockScore::Companies.new(self)
-      @candidate = BlockScore::Candidates.new(self)
-      @watchlist = BlockScore::Watchlists.new(self)
+      @candidates = BlockScore::Candidates.new(self)
+      @watchlists = BlockScore::Watchlists.new(self)
       @error_handler = BlockScore::ErrorHandler.new
 
       options[:base_uri] ||= "https://api.blockscore.com"
-      options[:headers] = { 'Accept' => 'application/vnd.blockscore+json;version=4' }
+      options[:headers] = { 
+        'Accept' => 'application/vnd.blockscore+json;version=4',
+        'User-Agent' => 'blockscore-ruby/4.0.0 (https://github.com/BlockScore/blockscore-ruby)'
+      }
       
       options.each do |k,v|
         self.class.send k, v
