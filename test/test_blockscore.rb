ac93dcf25172ec103b7fc9e3322e76ebecba1cb1
Alain Meier <me@alainmeier.com>
2014-11-17 01:16:14 -0800

    Make tests reflect new plural models

--- test/test_blockscore.rb
+++ test/test_blockscore.rb
@@ -14,7 +14,7 @@ class TestBlockScore < Test::Unit::TestCase
 
   context "a watchlist" do
     should "return search watchlists" do
-      response = @@client.watchlist.search(@@candidate_id)
+      response = @@client.watchlists.search(@@candidate_id)
       assert_equal 200, response.code
     end
   end
@@ -32,7 +32,7 @@ class TestBlockScore < Test::Unit::TestCase
        :address_city => "Cupertino",
        :address_country_code => "US"
       }
-      response = @@client.candidate.create(watchlist_params)
+      response = @@client.candidates.create(watchlist_params)
       assert_equal 201, response.code
     end
 
@@ -41,32 +41,32 @@ class TestBlockScore < Test::Unit::TestCase
         :date_of_birth => "1945-05-08",
         :name_middle => "Jones"
       }
-      response = @@client.candidate.edit(@@candidate_id, watchlist_params)
+      response = @@client.candidates.edit(@@candidate_id, watchlist_params)
       assert_equal 200, response.code
     end
 
     should "return retrieve a candidate" do
-      response = @@client.candidate.retrieve(@@candidate_id)
+      response = @@client.candidates.retrieve(@@candidate_id)
       assert_equal 200, response.code
     end
 
     should "return a list of candidates" do
-      response = @@client.candidate.all
+      response = @@client.candidates.all
       assert_equal 200, response.code
     end
 
     should "return a history of a candidate" do
-      response = @@client.candidate.history(@@candidate_id)
+      response = @@client.candidates.history(@@candidate_id)
       assert_equal 200, response.code
     end
 
     should "return the hits of a candidate" do
-      response = @@client.candidate.hits(@@candidate_id)
+      response = @@client.candidates.hits(@@candidate_id)
       assert_equal 200, response.code
     end
 
     should "return delete a candidate" do 
-      response = @@client.candidate.delete(@@candidate_id)
+      response = @@client.candidates.delete(@@candidate_id)
       assert_equal 200, response.code
     end
 
@@ -74,22 +74,22 @@ class TestBlockScore < Test::Unit::TestCase
 
   context "a company" do
     should "return a list of companies" do
-      response = @@client.company.all
+      response = @@client.companies.all
       assert_equal 200, response.code
     end
 
     should "return count = 2 companies" do
-      response = @@client.company.all(count = 2)
+      response = @@client.companies.all(count = 2)
       assert_equal 200, response.code
     end
 
     should "return count=2 offset=2 companies" do
-      response = @@client.company.all(count = 2, offset = 2)
+      response = @@client.companies.all(count = 2, offset = 2)
       assert_equal 200, response.code
     end
 
     should "return a single company" do
-      response = @@client.company.retrieve(@@company_id)
+      response = @@client.companies.retrieve(@@company_id)
       assert_equal 200, response.code
     end
 
@@ -117,7 +117,7 @@ class TestBlockScore < Test::Unit::TestCase
         :address_country_code => "US"
       }
 
-      response = @@client.company.create(company_params)
+      response = @@client.companies.create(company_params)
 
       assert_equal 201, response.code
     end
@@ -170,27 +170,27 @@ class TestBlockScore < Test::Unit::TestCase
 
   context "a question set" do
     should "return create a question set" do
-      response = @@client.question_set.create(@@person_id)
+      response = @@client.question_sets.create(@@person_id)
       assert_equal 201, response.code
     end
 
     should "return a single question set" do
-      response = @@client.question_set.retrieve(@@question_set_id)
+      response = @@client.question_sets.retrieve(@@question_set_id)
       assert_equal 200, response.code
     end
 
     should "return a list of question sets" do
-      response = @@client.question_set.all
+      response = @@client.question_sets.all
       assert_equal 200, response.code
     end
 
     should "return count = 2" do
-      response = @@client.question_set.all(count = 2)
+      response = @@client.question_sets.all(count = 2)
       assert_equal 200, response.code
     end
 
     should "return count = 2 offset = 2" do
-      response = @@client.question_set.all(count = 2, offset = 2)
+      response = @@client.question_sets.all(count = 2, offset = 2)
       assert_equal 200, response.code
     end
 
@@ -218,7 +218,7 @@ class TestBlockScore < Test::Unit::TestCase
           }
         ]
 
-      response = @@client.question_set.score(@@question_set_id, @answers)
+      response = @@client.question_sets.score(@@question_set_id, @answers)
 
       assert_equal 201, response.code
     end
