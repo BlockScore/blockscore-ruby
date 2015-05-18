a470fb97791f9dd1139e07f4db92ed0ed716d364
Connor Jacobsen <jacobsen.connor@gmail.com>
2015-05-18 16:19:34 -0700

    fix url generation bug

--- lib/blockscore/actions/delete.rb
+++ lib/blockscore/actions/delete.rb
@@ -2,7 +2,7 @@ module BlockScore
   module Actions
     module Delete
       def delete(id, options)
-        self.class.delete "#{self.class.endpoint}/#{id}", {}
+        self.class.delete "#{self.class.endpoint}#{id}", {}
       end
     end
   end
