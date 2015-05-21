57ad3458acb1b4a078c8839fe435df95fd921243
Connor Jacobsen <jacobsen.connor@gmail.com>
2015-05-20 20:50:11 -0700

    fix unused method argument in actions::delete

--- lib/blockscore/actions/delete.rb
+++ lib/blockscore/actions/delete.rb
@@ -1,8 +1,8 @@
 module BlockScore
   module Actions
     module Delete
-      def delete(id, options)
-        self.class.delete "#{self.class.endpoint}#{id}", {}
+      def delete(id, options = {})
+        self.class.delete "#{self.class.endpoint}#{id}", options
       end
     end
   end
