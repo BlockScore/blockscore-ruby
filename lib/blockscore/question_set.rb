76127404e61c321f64737de74ee375098d80236b
Connor Jacobsen <jacobsen.connor@gmail.com>
2015-05-21 23:10:08 -0700

    use forwardable

--- lib/blockscore/question_set.rb
+++ lib/blockscore/question_set.rb
@@ -1,23 +1,20 @@
 require 'blockscore/actions/create'
 require 'blockscore/actions/retrieve'
 require 'blockscore/actions/all'
+require 'forwardable'
 
 module BlockScore
   class QuestionSet < Base
+    extend Forwardable
+
     include BlockScore::Actions::Create
     include BlockScore::Actions::Retrieve
     include BlockScore::Actions::All
 
+    def_delegators 'self.class', :retrieve, :all
+
     def create
       self.class.create(:person_id => person_id)
-    end
-
-    def retrieve(id)
-      self.class.retrieve(id)
-    end
-
-    def all(options = {})
-      self.class.all(options)
     end
 
     def score(answers)
