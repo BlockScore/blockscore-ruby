bbe7be7f3163d97ec1b5a11c2d728c781f0a40e9
Connor Jacobsen <jacobsen.connor@gmail.com>
2015-05-22 15:52:46 -0700

    dry up candidate_test some

--- test/candidate_test.rb
+++ test/candidate_test.rb
@@ -3,27 +3,33 @@ require File.expand_path(File.join(__FILE__, '../test_helper'))
 class CandidateResourceTest < Minitest::Test
   include ResourceTest
 
-  def test_history
+  def member_test(member, response_type)
     candidate = TestClient.create_candidate
-    response = candidate.history
-    assert_equal Array, response.class
+    response = candidate.send(member)
+    assert_equal response_type, response.class
+  end
+
+  def create_candidate
+    TestClient.create_candidate
+  end
+
+  def test_history
+    member_test(:history, Array)
   end
 
   def test_hits
-    candidate = TestClient.create_candidate
-    response = candidate.hits
-    assert_equal Array, response.class
+    member_test(:hits, Array)
   end
 
   def test_update
-    candidate = TestClient.create_candidate
+    candidate = create_candidate
     candidate.name_first = 'Chris'
     assert_equal true, candidate.save
     assert_equal candidate.name_first, 'Chris'
   end
 
   def test_delete
-    candidate = TestClient.create_candidate
+    candidate = create_candidate
     candidate.delete
     assert_equal candidate.deleted, true
   end
