a2a3dfba3b89bb65587027c867053af33187a4fb
Connor Jacobsen <jacobsen.connor@gmail.com>
2015-05-21 21:58:55 -0700

    consistent error initialization -- from options hash with fetch

--- lib/blockscore/errors/invalid_request_error.rb
+++ lib/blockscore/errors/invalid_request_error.rb
@@ -4,9 +4,26 @@ module BlockScore
   class InvalidRequestError < BlockScoreError
     attr_accessor :param
 
-    def initialize(message, param, http_status=nil, http_body=nil, json_body=nil)
-      super(message, http_status, http_body, json_body)
-      @param = param
+    # Public: Creates a new instance of BlockScore::InvalidRequestError.
+    #
+    # Optional parameters:
+    #
+    # message - The String error message to report.
+    # http_status - The Fixnum HTTP status code (usually 4xx or 5xx).
+    # http_body - The body of the HTTP request.
+    # json_body - The JSON body of the HTTP request.
+    # param - The parameter which was invalid.
+    #
+    # Examples
+    #
+    # begin
+    #   response = BlockScore::Person.create(...)
+    # rescue BlockScore::InvalidRequestError => e
+    #   puts "ERROR: #{e.message} with code #{e.http_status}"
+    # end
+    def initialize(options = {})
+      super
+      @param = options.fetch :param, nil
     end
   end
 end
