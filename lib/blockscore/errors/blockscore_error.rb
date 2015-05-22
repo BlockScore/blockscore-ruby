30205b252661364a35243c7cfba1274a4a9c1ce1
Connor Jacobsen <jacobsen.connor@gmail.com>
2015-05-21 21:39:42 -0700

    use an options hash with fetch

--- lib/blockscore/errors/blockscore_error.rb
+++ lib/blockscore/errors/blockscore_error.rb
@@ -5,11 +5,26 @@ module BlockScore
     attr_reader :http_body
     attr_reader :json_body
 
-    def initialize(message=nil, http_status=nil, http_body=nil, json_body=nil)
-      @message = message
-      @http_status = http_status
-      @http_body = http_body
-      @json_body = json_body
+    # Public: Creates a new instance of BlockScore::InvalidRequestError.
+    #
+    # Optional parameters:
+    #
+    # message - The String error message to report.
+    # http_status - The Fixnum HTTP status code (usually 4xx or 5xx).
+    # http_body - The body of the HTTP request.
+    # json_body - The JSON body of the HTTP request.
+    # param - The parameter which was invalid.
+    #
+    # While BlockScoreError can be instantiated, the more meaningful
+    # error classes are its subclasses:
+    # InvalidRequestError - Indicates a malformed request (HTTP 400 or 404)
+    # APIError - Indicates an error on the server side (HTTP 5xx)
+    # AuthenticationError - Indicates an authentication error (HTTP 401)
+    def initialize(options = {})
+      @message = options.fetch :message, nil
+      @http_status = options.fetch :http_status, nil
+      @http_body = options.fetch :http_body, nil
+      @json_body = options.fetch :json_body, nil
     end
 
     def to_s
