93b332d73e21c2b2b21fc2cdddf2bf0b757c6343
Connor Jacobsen <jacobsen.connor@gmail.com>
2015-05-26 00:13:29 -0700

    add usable inflections replacements

--- lib/blockscore/util.rb
+++ lib/blockscore/util.rb
@@ -36,16 +36,50 @@ module BlockScore
     end
 
     def self.to_plural(str)
+      if str.end_with? 'y'
+        str[0..-2] + 'ies'
+      elsif str.end_with? 'h'
+        str + 'es'
+      elsif str == 'person'
+        'people'
+      else
+        str + 's'
+      end
     end
 
-    def self.to_constant(str)
-      # candidate / candidates
-      # person / people
-      # company / companies
-      # question_set / question_sets
+    def self.to_constant(camel_cased_word)
+      names = camel_cased_word.split('::')
+
+      # Trigger a built-in NameError exception including the ill-formed constant in the message.
+      Object.const_get(camel_cased_word) if names.empty?
+
+      # Remove the first blank element in case of '::ClassName' notation.
+      names.shift if names.size > 1 && names.first.empty?
+
+      names.inject(Object) do |constant, name|
+        if constant == Object
+          constant.const_get(name)
+        else
+          candidate = constant.const_get(name)
+          next candidate if constant.const_defined?(name, false)
+          next candidate unless Object.const_defined?(name)
+
+          # Go down the ancestors to check if it is owned directly. The check
+          # stops when we reach Object or the end of ancestors tree.
+          constant = constant.ancestors.inject do |const, ancestor|
+            break const    if ancestor == Object
+            break ancestor if ancestor.const_defined?(name, false)
+            const
+          end
+
+          # owner is in Object, so raise
+          constant.const_get(name, false)
+        end
+      end
     end
 
     def self.to_camelcase(str)
+      str.split('_').map { |i| i.capitalize }.join('')
     end
 
     def self.to_underscore(str)
--- test/util_test.rb
+++ test/util_test.rb
@@ -8,4 +8,26 @@ class UtilTest < Minitest::Test
     assert_equal BlockScore::Util.to_underscore(''), ''
     assert_equal BlockScore::Util.to_underscore('FooBarBaz'), 'foo_bar_baz'
   end
+
+  def test_constant
+    assert_equal BlockScore::Util.to_constant('BlockScore::Person'), BlockScore::Person
+    assert_equal BlockScore::Util.to_constant('BlockScore::Company'), BlockScore::Company
+    assert_equal BlockScore::Util.to_constant('BlockScore::QuestionSet'), BlockScore::QuestionSet
+  end
+
+  def test_plural
+    assert_equal BlockScore::Util.to_plural('whale'), 'whales'
+    assert_equal BlockScore::Util.to_plural('rock'), 'rocks'
+    assert_equal BlockScore::Util.to_plural('test'), 'tests'
+    assert_equal BlockScore::Util.to_plural('company'), 'companies'
+    assert_equal BlockScore::Util.to_plural('person'), 'people'
+    assert_equal BlockScore::Util.to_plural('beach'), 'beaches'
+  end
+
+  def test_camelcase
+    assert_equal BlockScore::Util.to_camelcase('screaming_snake_case'), 'ScreamingSnakeCase'
+    assert_equal BlockScore::Util.to_camelcase('snake_case'), 'SnakeCase'
+    assert_equal BlockScore::Util.to_camelcase('foo'), 'Foo'
+    assert_equal BlockScore::Util.to_camelcase(''), ''
+  end
 end
