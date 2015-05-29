d7771667ce30f0ae070f8a7d655a09379a2e1ae4
Connor Jacobsen <jacobsen.connor@gmail.com>
2015-05-28 22:14:21 -0700

    start cleaning up Responder

--- lib/blockscore/responder.rb
+++ lib/blockscore/responder.rb
@@ -11,21 +11,7 @@ module BlockScore
       when 200, 201
         json_obj = Util.parse_json(response.body)
 
-        if json_obj.respond_to?(:key?) && json_obj.key?(:matches)
-          Util.create_watchlist_hit_array json_obj[:matches]
-        elsif json_obj.class == Array
-          build_response_from_arr json_obj
-        elsif json_obj[:object] == 'list'
-          data = json_obj[:data]
-
-          if data.first.class == Hash && data.first.key?(:matching_info)
-            Util.create_watchlist_hit_array data
-          else
-            build_response_from_arr data
-          end
-        else
-          Util.create_object resource, json_obj
-        end
+        dispatch_success(json_obj)
       else
         Util.handle_api_error response
       end
@@ -35,6 +21,43 @@ module BlockScore
 
     def build_response_from_arr(arr_obj)
       arr_obj.map { |obj| Util.create_object(resource, obj) }
+    end
+
+    def dispatch_success(json)
+      if watchlist_search?(json)
+        Util.create_watchlist_hit_array json[:matches]
+      elsif resource_array?(json)
+        build_response_from_arr json
+      elsif resource_index?(json)
+        index_response(json)
+      else # single object
+        Util.create_object resource, json
+      end
+    end
+
+    # candidates#search endpoint
+    def watchlist_search?(data)
+      data.respond_to?(:key?) && data.key?(:matches)
+    end
+
+    # hash style list format
+    def resource_index?(data)
+      data.class == Hash && data[:object] == 'list'
+    end
+
+    def index_response(json)
+      data = json[:data]
+
+      if data.first.class == Hash && data.first.key?(:matching_info)
+        Util.create_watchlist_hit_array data
+      else
+        build_response_from_arr data
+      end
+    end
+
+    # array formatted response
+    def resource_array?(data)
+      data.class == Array
     end
   end
 end
