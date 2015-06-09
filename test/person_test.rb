686da6f09e188fcbe7660f759e57138b0d84688e
Connor Jacobsen <jacobsen.connor@gmail.com>
2015-06-08 21:45:07 -0700

    test for hash access with ruby object syntax

--- test/person_test.rb
+++ test/person_test.rb
@@ -2,4 +2,16 @@ require File.expand_path(File.join(__FILE__, '../test_helper'))
 
 class PersonResourceTest < Minitest::Test
   include ResourceTest
+
+  def test_details_access
+    person = create_person
+    details = person.details
+
+    assert_respond_to details, :address
+    assert_respond_to details, :address_risk
+    assert_respond_to details, :identification
+    assert_respond_to details, :date_of_birth
+    assert_respond_to details, :ofac
+    assert_respond_to details, :pep
+  end
 end
