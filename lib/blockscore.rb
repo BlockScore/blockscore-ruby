a33b41bf43c8d67e6558cd9279c42eff3feabdf3
Connor Jacobsen <jacobsen.connor@gmail.com>
2015-06-09 12:40:17 -0700

    reorganize requires

--- lib/blockscore.rb
+++ lib/blockscore.rb
@@ -9,23 +9,26 @@ require 'blockscore/actions/create'
 require 'blockscore/actions/delete'
 require 'blockscore/actions/retrieve'
 require 'blockscore/actions/update'
-require 'blockscore/base'
-require 'blockscore/candidate'
-require 'blockscore/company'
-require 'blockscore/connection'
-require 'blockscore/dispatch'
+
 require 'blockscore/errors/api_connection_error'
 require 'blockscore/errors/api_error'
 require 'blockscore/errors/authentication_error'
 require 'blockscore/errors/error'
 require 'blockscore/errors/invalid_request_error'
-require 'blockscore/fingerprint'
+
+require 'blockscore/base'
+require 'blockscore/candidate'
+require 'blockscore/company'
 require 'blockscore/person'
 require 'blockscore/question_set'
+require 'blockscore/watchlist_hit'
+
+require 'blockscore/connection'
+require 'blockscore/dispatch'
+require 'blockscore/fingerprint'
 require 'blockscore/response'
 require 'blockscore/util'
 require 'blockscore/version'
-require 'blockscore/watchlist_hit'
 
 module BlockScore
   def self.api_key=(api_key)
