d3983b9c2cdaa650c52bc1fc6477294ec44233b8
John Backus <jbackus@stanford.edu>
2015-08-27 16:26:55 -0700

    Refactor response.rb extracting response conditions

--- spec/support/response.rb
+++ spec/support/response.rb
@@ -1,29 +1,45 @@
+def error_response?(stub)
+  BlockScore::StubbedResponse::Error::MAP.key?(stub.id)
+end
+
+def create_response?(stub)
+  stub.http_method == :post && stub.action.nil?
+end
+
+def list_response?(stub)
+  stub.id.equal?(:no_id) && stub.http_method == :get || stub.action == 'hits'
+end
+
+def history_response?(stub)
+  stub.action == 'history'
+end
+
 def handle_test_response(stub)
-  if BlockScore::StubbedResponse::Error::MAP.key?(stub.id)
+  if error_response?(stub)
     BlockScore::StubbedResponse::Error.new(stub.id).response
   else
-    status = response_status stub.action, stub.http_method
-    body = response_body stub.id, stub.action, stub.factory_name, stub.query_params, stub.http_method
+    status = response_status(stub)
+    body = response_body(stub)
 
     build_test_response status, body
   end
 end
 
-def response_status(action, http_method)
-  if http_method == :post && action.nil?
+def response_status(stub)
+  if create_response?(stub)
     201
   else
     200
   end
 end
 
-def response_body(id, action, factory_name, options, http_method)
-  if id.equal?(:no_id) && http_method == :get || action == 'hits'
-    BlockScore::StubbedResponse::List.new(factory_name, options).response.fetch(:body)
-  elsif action == 'history'
-    BlockScore::StubbedResponse::History.new(factory_name).response.fetch(:body)
+def response_body(stub)
+  if list_response?(stub)
+    BlockScore::StubbedResponse::List.new(stub.factory_name, stub.query_params).response.fetch(:body)
+  elsif history_response?(stub)
+    BlockScore::StubbedResponse::History.new(stub.factory_name).response.fetch(:body)
   else
-    BlockScore::StubbedResponse::Retrieve.new(factory_name).response.fetch(:body)
+    BlockScore::StubbedResponse::Retrieve.new(stub.factory_name).response.fetch(:body)
   end
 end
 
