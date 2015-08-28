b3fcdb1e61fb02cab5cdb1400ea4389d5b7afa98
John Backus <jbackus@stanford.edu>
2015-08-27 22:59:39 -0700

    Refactor adding shared_context 'api request'

---
+++ spec/support/api_request_behavior.rb
@@ -0,0 +1,19 @@
+module BlockScore
+  RSpec.shared_context 'api request' do
+    let(:base_url)    { "https://#{Spec::API_KEY}:@api.blockscore.com"   }
+    let(:url)         { base_url + uri                                   }
+    let(:_expected)   { respond_to?(:expected) ? expected : { body: {} } }
+    subject(:request) { a_request(http_method, url).with(_expected)      }
+
+    it { should have_been_made }
+  end
+
+  %w(get post patch delete).each do |verb|
+    RSpec.shared_context verb.upcase do
+      let(:http_method) { verb.to_sym }
+
+      include_context 'api request'
+    end
+  end
+
+end
--- spec/unit/blockscore/candidate_spec.rb
+++ spec/unit/blockscore/candidate_spec.rb
@@ -2,19 +2,29 @@ module BlockScore
   RSpec.describe Candidate do
     let(:api_stub) { @api_stub }
 
-    it 'updates' do
-      candidate = create(:candidate_params)
-      candidate.name_first = 'Chris'
-      expect(candidate.save).to eq(true)
-      assert_requested(api_stub, times: 2)
-      expect('Chris').to eq(candidate.name_first)
+    let(:candidate) { create(:candidate_params) }
+
+    context 'updating' do
+      before do
+        candidate.name_first = 'Chris'
+        candidate.save
+      end
+
+      let(:uri)         { "/candidates/#{candidate.id}"                     }
+      let(:http_method) { :patch                                            }
+      let(:expected)    { { body: hash_including('name_first' => 'Chris') } }
+
+      it { expect(candidate.name_first).to eql('Chris') }
+      include_context 'api request'
     end
 
-    it 'deletes' do
-      candidate = create(:candidate_params)
-      candidate.delete
-      assert_requested(api_stub, times: 2)
-      expect(true).to eq(candidate.deleted)
+    describe '#delete' do
+      before { candidate.delete }
+
+      let(:uri)         { "/candidates/#{candidate.id}" }
+      let(:http_method) { :delete                       }
+
+      include_context 'api request'
     end
   end
 end
