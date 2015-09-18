dd63540d91dad7850d9ad15c875d017f4b1df214
Zane Wolfgang Pickett <Zane@BlockScore.com>
2015-09-18 11:05:44 -0700

    Update vcr_setup based on feedback

--- spec/vcr_setup.rb
+++ spec/vcr_setup.rb
@@ -2,40 +2,52 @@ require 'vcr'
 require 'rspec'
 
 module BlockScore
+  RECORD_MODE_KEY     = 'VCR_RECORD_MODE'
+  ENVIRONMENT_API_KEY = 'BLOCKSCORE_TEST_KEY'
+  PLACEHOLDER_API_KEY = 'BLOCKSCORE_TEST_KEY'
+
+  extend self
+
+  RSpec.configure do |config|
+    config.before(:suite) do
+      BlockScore.api_key = ENV.fetch(ENVIRONMENT_API_KEY, PLACEHOLDER_API_KEY)
+    end
+
+    config.around(:each) do |example|
+      record_option = ENV.fetch(RECORD_MODE_KEY, :none).to_sym
+      options = example.metadata.fetch(:vcr, record: record_option)
+      BlockScore.run_vcr_example(options, example)
+    end
+  end
+
   VCR.configure do |c|
     c.cassette_library_dir = 'spec/cassettes'
     c.hook_into :webmock
     c.configure_rspec_metadata!
     c.ignore_hosts 'codeclimate.com'
 
-    c.filter_sensitive_data('BLOCKSCORE_TEST_KEY') { ENV['BLOCKSCORE_TEST_KEY'] }
+    c.filter_sensitive_data(PLACEHOLDER_API_KEY) { ENV[ENVIRONMENT_API_KEY] }
   end
 
-  RSpec.configure do |config|
-    config.before(:suite) do
-      BlockScore.api_key = ENV.fetch('BLOCKSCORE_TEST_KEY', 'BLOCKSCORE_TEST_KEY')
+  def run_vcr_example(options, example)
+    if options.fetch(:record).equal?(:skip)
+      VCR.turned_off(&example)
+    else
+      path = BlockScore.build_path(example.metadata.fetch(:full_description))
+
+      VCR.use_cassette(path, options, &example)
     end
+  end
 
-    config.around(:each) do |example|
-      options = example.metadata[:vcr] || {
-        record: (ENV['VCR_RECORD_MODE'] ? ENV['VCR_RECORD_MODE'].to_sym : :none)
-      }
-
-      if options[:record] == :skip
-        VCR.turned_off(&example)
-      else
-        name = example.metadata[:full_description]
-          .split(/\s+/, 2)
-          .join('/')
-          .underscore
-          .gsub(/[.#?]/, '/')
-          .gsub(%r(\/+), '/')
-          .strip
-          .tr(' ', '_')
-          .gsub(%r([/_ ]$), '')
-
-        VCR.use_cassette(name, options, &example)
-      end
-    end
+  def build_path(full_description)
+    full_description
+      .split(/\s+/, 2)
+      .join('/')
+      .underscore
+      .gsub(/[.#?]/, '/')
+      .gsub(%r(\/+), '/')
+      .strip
+      .tr(' ', '_')
+      .gsub(%r([/_ ]$), '')
   end
 end
