9d7940e622f8765df4e4222ab886e2b9cac53544
John Backus <johncbackus@gmail.com>
2016-01-21 14:13:38 -0800

    Add better CI handling for number of circle nodes

--- circleci.sh
+++ circleci.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 RUBY_VERSIONS=(2.0.0-p645 2.1.6 2.2.2)
 CIRCLE_NODE_RUBY_VERSION="${RUBY_VERSIONS[$CIRCLE_NODE_INDEX]}"
+NUM_RUBIES=${#RUBY_VERSIONS[@]}
+
+# Fail build if # ruby versions is greater than number of containers
+if [ $CIRCLE_NODE_TOTAL -lt $NUM_RUBIES ]; then
+  echo "Cannot test $NUM_RUBIES ruby versions on $CIRCLE_NODE_TOTAL containers"
+  exit 1
+fi
+
+# Exit early if there are more containers than ruby versions to test
+if [ $CIRCLE_NODE_INDEX -gt $(($NUM_RUBIES-1)) ]; then
+  echo "Ignoring extra container"
+  exit 0
+fi
 
 if [[ "$1" = "build" ]]
 then
