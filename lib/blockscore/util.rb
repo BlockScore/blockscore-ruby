bf584dd05fa52d00b8743dce91205bb3e757e7eb
Tim Chambers <tim@possibilogy.com>
2016-02-07 11:31:12 -0800

    Fix Util's rubocop max line length recommendation

--- lib/blockscore/util.rb
+++ lib/blockscore/util.rb
@@ -10,6 +10,10 @@ module BlockScore
       'watchlist_hit' => 'watchlist_hits'
     }
 
+    PARSE_ERROR =
+      'An error has occurred. ' \
+        'If this problem persists, please message support@blockscore.com.'
+
     def parse_json!(json_obj)
       JSON.parse(json_obj, symbolize_names: true)
     end
@@ -17,7 +21,7 @@ module BlockScore
     def parse_json(json_obj)
       parse_json! json_obj
     rescue JSON::ParserError
-      raise Error, 'An error has occurred. If this problem persists, please message support@blockscore.com.'
+      raise Error, PARSE_ERROR
     end
 
     def create_object(resource, options = {})
@@ -36,7 +40,8 @@ module BlockScore
     def to_constant(camel_cased_word)
       names = camel_cased_word.split('::')
 
-      # Trigger a built-in NameError exception including the ill-formed constant in the message.
+      # Trigger a built-in NameError exception including
+      # the ill-formed constant in the message.
       Object.const_get(camel_cased_word) if names.empty?
 
       # Remove the first blank element in case of '::ClassName' notation.
