70b8bb3d3ab6ed7e06adfd75e3f27dddf9ff86b5
Tim Chambers <tim@possibilogy.com>
2016-02-08 10:22:56 -0800

    Add missing test spec for BlockScore::Collection::Member
    This originally had an erroneous describe block for .save rather than #save (instance method). In addition the test example block was empty (no test).

--- spec/unit/blockscore/collection/member_spec.rb
+++ spec/unit/blockscore/collection/member_spec.rb
@@ -1,7 +1,15 @@
 module BlockScore
   class Collection
     RSpec.describe Member do
-      describe '.save' do
+      describe '#save' do
+        let(:person)        { BlockScore::Person.new(attributes_for(:person)) }
+        let(:question_set)  { QuestionSet.new }
+        subject(:member)    { described_class.new(person, question_set) }
+
+        before { member.save }
+
+        it { expect(person).to be_persisted }
+        its(:class) { should be described_class }
       end
     end
   end
