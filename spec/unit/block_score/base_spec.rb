738e2ac7f943561e869d53b12c01465f17783d8b
Tim Chambers <tim@possibilogy.com>
2016-02-13 06:59:41 -0800

    Add more comprehensive Base unit tests

--- spec/unit/block_score/base_spec.rb
+++ spec/unit/block_score/base_spec.rb
@@ -14,22 +14,88 @@ RSpec.describe BlockScore::Base do
       subject(:candidate) { create(:candidate, name_first: 'John') }
       before do
         candidate.name_first = 'Jane'
-        candidate.save
+        expect(candidate.save).to be true
       end
 
       it { is_expected.to be_persisted }
       its(:name_first) { is_expected.to eql 'Jane' }
     end
+
+    context 'when encountering a deleted item' do
+      subject(:candidate) { create(:candidate, name_first: 'John') }
+
+      it 'raises and error when previously deleted' do
+        candidate.delete
+        expect(candidate.save).to be false
+      end
+    end
+  end
+
+  describe '#save!' do
+    subject(:candidate) { create(:candidate, name_first: 'John') }
+    context 'when updating an existing candidate' do
+      it 'successfully saves an alteration' do
+        candidate.name_first = 'Jane'
+
+        expect { candidate.save! } .to_not change(candidate, :id)
+        expect(candidate.persisted?).to be true
+        expect(candidate.name_first).to eql 'Jane'
+      end
+    end
+
+    context 'when encountering a deleted item' do
+      it 'raises an error when previously deleted' do
+        candidate.delete
+        expect { candidate.save! }
+          .to raise_error(BlockScore::Error, 'candidate is already deleted')
+      end
+    end
   end
 
   describe '#refresh' do
     subject(:candidate) { create(:candidate, name_last: 'Smith') }
+    context 'refresh an existing resource' do
+      before do
+        candidate.name_last = 'John'
+        candidate.refresh
+      end
+
+      its(:name_last) { is_expected.to eql 'Smith' }
+    end
+
+    context 'refresh a non saved item' do
+      subject(:candidate) { build(:candidate, name_last: 'Smith') }
+
+      it 'is false return if not refreshable' do
+        expect { candidate.refresh }
+          .to raise_error(ArgumentError, 'ID must be supplied')
+      end
+    end
+  end
+
+  describe '#id' do
+    subject(:candidate) { create(:candidate, name_last: 'Smith') }
+
+    its(:id) { is_expected.to be_an_instance_of String }
+  end
+
+  describe '#wrap_hash' do
+    subject(:person) { create(:person, name_last: 'Smith') }
+    let(:expectation) do
+      OpenStruct.new(address: 'no_match',
+                     address_risk: 'low',
+                     identification: 'no_match',
+                     date_of_birth: 'not_found',
+                     ofac: 'no_match',
+                     pep: 'no_match')
+    end
     before do
-      candidate.name_last = 'John'
-      candidate.refresh
+      person.name_last = 'John'
+      person.refresh
     end
 
     its(:name_last) { is_expected.to eql 'Smith' }
+    its(:details) { is_expected.to eql expectation }
   end
 
   describe '#inspect' do
