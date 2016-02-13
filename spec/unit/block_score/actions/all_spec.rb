41560de3b1da12287a8e6fd7dc34cb179c0b5619
Tim Chambers <tim@possibilogy.com>
2016-02-13 07:01:06 -0800

    Add ::All spec

---
+++ spec/unit/block_score/actions/all_spec.rb
@@ -0,0 +1,18 @@
+require 'faker'
+
+RSpec.describe BlockScore::Actions::All do
+  describe '.all' do
+    let(:uniq_token_one) { '4a2c019a1e3c33442644d9f52c7a93f7' }
+    let(:uniq_token_two) { 'ba9a680671c6c006c6105aa450837668' }
+    subject(:people) { BlockScore::Person.all }
+
+    before do
+      create(:person, name_first: 'John', name_last: uniq_token_one)
+      create(:person, name_first: 'John', name_last: uniq_token_two)
+    end
+
+    it { is_expected.not_to be_empty }
+    it { expect(people[0].name_last).to eql uniq_token_two }
+    it { expect(people[1].name_last).to eql uniq_token_one }
+  end
+end
