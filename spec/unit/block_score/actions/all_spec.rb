57c472e99296d33c5cdce579ffc994766fd298ee
Tim Chambers <tim@possibilogy.com>
2016-02-14 14:03:15 -0800

    Favor let! over before blocks when defining context

--- spec/unit/block_score/actions/all_spec.rb
+++ spec/unit/block_score/actions/all_spec.rb
@@ -4,12 +4,9 @@ RSpec.describe BlockScore::Actions::All do
   describe '.all' do
     let(:uniq_token_one) { '4a2c019a1e3c33442644d9f52c7a93f7' }
     let(:uniq_token_two) { 'ba9a680671c6c006c6105aa450837668' }
+    let!(:p1) { create(:person, name_first: 'John', name_last: uniq_token_one) }
+    let!(:p2) { create(:person, name_first: 'John', name_last: uniq_token_two) }
     subject(:people) { BlockScore::Person.all }
-
-    before do
-      create(:person, name_first: 'John', name_last: uniq_token_one)
-      create(:person, name_first: 'John', name_last: uniq_token_two)
-    end
 
     it { is_expected.not_to be_empty }
     it { expect(people[0].name_last).to eql uniq_token_two }
