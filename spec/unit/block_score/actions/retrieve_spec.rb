507df3b8b61bc23034a2b64b71450e7180ea816a
Tim Chambers <tim@possibilogy.com>
2016-02-19 08:50:32 -0800

    Add specs for .included logic
    This should complete the mutation coverage for these actions

--- spec/unit/block_score/actions/retrieve_spec.rb
+++ spec/unit/block_score/actions/retrieve_spec.rb
@@ -25,4 +25,18 @@ RSpec.describe BlockScore::Actions::Retrieve, vcr: true do
       end
     end
   end
+
+  describe '.included' do
+    subject { object }
+    let(:object) { described_class }
+    let(:klass) { Class.new }
+
+    it 'extends the klass' do
+      expect(klass.singleton_class)
+        .not_to include(described_class::ClassMethods)
+      klass.send(:include, subject)
+      expect(klass.singleton_class)
+        .to include(described_class::ClassMethods)
+    end
+  end
 end
